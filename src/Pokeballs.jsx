a64ca0240f04059a56070b9569897db2c734efd2
Romain <romain.herault@les-tilleuls.coop>
2023-11-11 18:35:03 +0100

    perf: making pokeball in instances

--- src/Pokeballs.jsx
+++ src/Pokeballs.jsx
@@ -1,14 +1,13 @@
 import * as THREE from 'three'
 import { useRef, useState } from 'react'
 import { Canvas, useThree, useFrame } from '@react-three/fiber'
-import { useGLTF, Environment } from '@react-three/drei'
+import { useGLTF, Environment, Instances, Instance } from '@react-three/drei'
 import { EffectComposer, DepthOfField } from '@react-three/postprocessing'
 
 function Pokeball({ index, z, speed }) {
   const ref = useRef()
   const { viewport, camera } = useThree()
   const { width, height } = viewport.getCurrentViewport(camera, [0, 0, -z])
-  const { nodes, materials } = useGLTF('/model/realistic-pokeball.glb')
 
   const [data] = useState({
     y: THREE.MathUtils.randFloatSpread(height * 2),
@@ -24,23 +23,21 @@ function Pokeball({ index, z, speed }) {
     if (data.y < -(height * (index === 0 ? 4 : 1))) data.y = (height * (index === 0 ? 4 : 1))
   })
 
-  return <mesh
-        ref={ref}
-        castShadow
-        receiveShadow
-        geometry={nodes.pokeball.geometry}
-        material={materials.skin}
-    />
+  return <Instance ref={ref} />
 }
 
 useGLTF.preload("/model/realistic-pokeball.glb");
 
 export default function Pokeballs({ speed = 1, count = 500, depth = 80, easing = (x) => Math.sqrt(1 - Math.pow(x - 1, 2)) }) {
+  const { nodes, materials } = useGLTF('/model/realistic-pokeball.glb')
+
   return (
     <Canvas gl={{ antialias: false }} dpr={[1, 1.5]} camera={{ position: [0, 0, 10], fov: 20, near: 0.01, far: depth + 15 }}>
       <color attach="background" args={['#1C4CBD']} />
       <spotLight position={[10, 20, 10]} penumbra={1} intensity={3} color="red" />
-      {Array.from({ length: count }, (_, i) => <Pokeball key={i} index={i} z={Math.round(easing(i / count) * depth)} speed={speed} /> /* prettier-ignore */)}
+      <Instances range={count} material={materials.skin} geometry={nodes.pokeball.geometry}>
+            {Array.from({ length: count }, (_, i) => <Pokeball key={i} index={i} z={Math.round(easing(i / count) * depth)} speed={speed} /> /* prettier-ignore */)}
+      </Instances>
       <Environment preset="sunset" />
       <EffectComposer disableNormalPass multisampling={0}>
         <DepthOfField target={[0, 0, 60]} focalLength={0.4} bokehScale={4} height={700} />
